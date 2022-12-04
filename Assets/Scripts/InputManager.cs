e169c88d8cd328452c351b110c9456b2fed9f2a1
Loïc Rohan <loicrohan@gmail.com>
2022-12-04 15:32:19 +0530

    Initial Commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+                RaycastHit hitInfo;
+                if(Physics.Raycast(ray,out hitInfo))
+                {
+                    if(hitInfo.collider != null)
+                    {
+                        Color color = new Color(Random.value, Random.value, Random.value);
+                        hitInfo.collider.gameObject.GetComponent<MeshRenderer>().material.color = color;
+                    }
+                }
+            }
+        }
+    }
+}
